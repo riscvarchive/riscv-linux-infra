3903720ed612852b550ced94616ae97a6089d534
Palmer Dabbelt <palmer@dabbelt.com>
2018-01-24 12:52:04 -0800

    Move to a RISC-V specific kernel.org repo

--- scripts/show-remote-url.bash
+++ scripts/show-remote-url.bash
@@ -15,7 +15,7 @@ done
 case "$remote"
 in
 kernel.org-torvalds) echo "git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git";;
-kernel.org-palmer) echo "git://git.kernel.org/pub/scm/linux/kernel/git/palmer/linux.git";;
+kernel.org-palmer) echo "git://git.kernel.org/pub/scm/linux/kernel/git/palmer/riscv-linux.git";;
 github.com-riscv) echo "git://github.com/riscv/riscv-linux.git";;
 *) echo "Unknown remote \"$remote\"">&2; exit 1;;
 esac
