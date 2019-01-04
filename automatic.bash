953aed5d7789fcee54d631c00ff10a35e91c1be0
Palmer Dabbelt <palmer@dabbelt.com>
2019-01-04 11:23:53 -0800

    This is no good
    Signed-off-by: Palmer Dabbelt <palmer@dabbelt.com>

--- automatic.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-ionice -c3 nice -n10 ./regenerate.bash --branch master "$@"
-ionice -c3 nice -n10 ./regenerate.bash --branch for-next "$@"
-#ionice -c3 nice -n10 ./regenerate.bash --branch riscv-all "$@"
